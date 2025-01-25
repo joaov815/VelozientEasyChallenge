4144618e3c1d5e99fddbd2f904230ffe2b4c03be
João Victor <souza@espressolabs.com.br>
2025-01-25 12:38:29 -0300

    feat(TwistedFizzBuzzHelper): Add GetFizzBuzz private method

--- TwistedFizzBuzz/TwistedFizzBuzzHelper.cs
+++ TwistedFizzBuzz/TwistedFizzBuzzHelper.cs
@@ -2,5 +2,36 @@
 
 public class TwistedFizzBuzzHelper
 {
+    private static string[] GetFizzBuzz(
+        int[] numbers,
+        Dictionary<int, string> numberPerToken,
+        bool shouldCombinateRules = false
+    )
+    {
+        int length = numbers.Length;
+        string[] result = new string[length];
 
+        for (int i = 0; i < length; i++)
+        {
+            int num = numbers[i];
+            string curToken = "";
+
+            foreach (var (ruleNum, token) in numberPerToken)
+            {
+                if (num % ruleNum == 0)
+                {
+                    curToken += string.IsNullOrEmpty(curToken) ? token : $"-{token}";
+
+                    if (!shouldCombinateRules)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            result[i] = string.IsNullOrEmpty(curToken) ? num.ToString() : curToken;
+        }
+
+        return result;
+    }
 }

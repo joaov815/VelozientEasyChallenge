91e7dddd20729ae99207c0a26e09fdd2f7e585fd
João Victor <souza@espressolabs.com.br>
2025-01-25 12:55:54 -0300

    feat(TwistedFizzBuzzHelper): Add FromRange static method

--- TwistedFizzBuzz/TwistedFizzBuzzHelper.cs
+++ TwistedFizzBuzz/TwistedFizzBuzzHelper.cs
@@ -2,12 +2,36 @@
 
 public class TwistedFizzBuzzHelper
 {
-    private static string[] GetFizzBuzz(
-        int[] numbers,
-        Dictionary<int, string> numberPerToken,
+    private static readonly Dictionary<int, string> defaultNumberPerToken = new()
+    {
+        { 3, "Fizz" },
+        { 5, "Buzz" },
+    };
+
+    public static string[] FromRange(
+        int start,
+        int end,
+        Dictionary<int, string>? numberPerToken = null,
         bool shouldCombinateRules = false
     )
     {
+        if (end < start)
+        {
+            throw new Exception("The end parameter should be higher than the start");
+        }
+
+        int[] rangeArray = [.. Enumerable.Range(start, end - start + 1)];
+
+        return GetFizzBuzz(rangeArray, numberPerToken, shouldCombinateRules);
+    }
+
+    private static string[] GetFizzBuzz(
+        int[] numbers,
+        Dictionary<int, string>? numberPerToken = null,
+        bool shouldCombinateRules = false
+    )
+    {
+        numberPerToken ??= defaultNumberPerToken;
         int length = numbers.Length;
         string[] result = new string[length];
 
